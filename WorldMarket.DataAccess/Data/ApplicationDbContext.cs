fd60105809c3176ee6374f4fcaaf8d6d1af56526
MrNoby47 <98368473+MrNoby47@users.noreply.github.com>
2023-07-17 17:38:51 +0800

    Identity & Application User

--- WorldMarket.DataAccess/Data/ApplicationDbContext.cs
+++ WorldMarket.DataAccess/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace WorldMarket.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }

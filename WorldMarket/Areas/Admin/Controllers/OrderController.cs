f3870850808e3bdf18fa8ad71b9e3ba7ef6878b3
MrNoby47 <98368473+MrNoby47@users.noreply.github.com>
2023-07-22 18:11:15 +0800

    Order Filter
    Order filter based on user type and order status

---
+++ WorldMarket/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Numerics;
+using System.Security.Claims;
+using WorldMarket.DataAccess.Repository.IRepository;
+using WorldMarket.Models;
+using WorldMarket.Utility;
+
+namespace WorldMarket.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        public IActionResult GetAll(string status)
+        {
+
+            IEnumerable<OrderHeader> orderHeaders;
+            if(User.IsInRole(SD.SD_Role_Admin) || User.IsInRole(SD.SD_Role_Emp))
+            {
+                orderHeaders = _unitOfWork.OrderHeaders.GetAll(includeProperties: "ApplicationUser");
+            }
+           else {
+                var identityUser = (ClaimsIdentity)User.Identity;
+                var claim = identityUser.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeaders = _unitOfWork.OrderHeaders.GetAll(x => x.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
+            }
+           
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.OrderStatusPending);
+                    break;
+                case "complete":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.OrderStatusShiped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.OrderStatusApproved);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.OrderStatusInProcess);
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            return Json(new { data = orderHeaders });
+
+        }
+        #endregion
+    }
+}
---
+++ WorldMarket/wwwroot/js/order.js
@@ -0,0 +1,55 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("pending")) {
+        loadDataTable("pending");
+    }
+    else {
+        if (url.includes("inprocess")) {
+            loadDataTable("inprocess");
+        }
+        else {
+            if (url.includes("complete")) {
+                loadDataTable("complete");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
+   
+});
+
+function loadDataTable(status) {
+    dataTable = $("#orderTable").DataTable({
+        "ajax": {
+            "url": "/Admin/Order/GetAll?status="+ status
+        },
+        "columns": [
+            { "data": "id","width":"5%" },
+            { "data": "name", "width": "25%" },
+            { "data": "orderTotal", "width": "15%" },
+            { "data": "paymentStatus", "width": "15%" },
+            {"data":  "phoneNumber", "width":"10%"},
+            { "data": "applicationUser.email", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                              <div class="w-75 btn-group" role="group">
+                                   <a class="btn btn-primary mx-2"  href="Admin/Order/Details?orderId=${data}" ><i class="bi bi-pencil-square"></i></a>
+                               </div>
+
+                            `
+                },
+                "width":"5%"
+            }
+        ]
+    });
+}

40facc0e47a59aef66d566c2ccc0b534fbd3cb46
MrNoby47 <98368473+MrNoby47@users.noreply.github.com>
2023-07-26 11:37:06 +0800

    before adding list properties

--- WorldMarket.Models/Product.cs
+++ WorldMarket.Models/Product.cs
@@ -11,32 +11,39 @@ namespace WorldMarket.Models
 {
     public class Product
     {
-        
+
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         [Required]
-        public string Description { get; set; }
-        [Required]
         [Range(1, 10000)]
         public double Price { get; set; }
         [Required]
+        public string Description { get; set; }
+        [Required]
         [Range(1, 10000)]
+        [Display(Name = "1 - 50")]
         public double ListPrice { get; set; }
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "51 - 99")]
         public double Price50 { get; set; }
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "100+")]
         public double Price100 { get; set; }
         [ValidateNever]
+        [Display(Name = "Image")]
         public string ImgUrl { get; set; }
+        public List<string>? Color { get; set; }
         [Required]
+        [Display(Name = "Cover")]
         public int CoverTypeId { get; set; }
         [ForeignKey("CoverTypeId")]
         [ValidateNever]
         public CoverType CoverType { get; set; }
         [Required]
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         [ValidateNever]
